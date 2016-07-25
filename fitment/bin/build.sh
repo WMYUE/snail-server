8fa8dfad5875ad011df4e57afb40ebf18cadf51f
chenlei <chenlei@comisys.net>
2016-07-25 20:20:33 +0800

    修改maven编译文件

--- fitment/bin/build.sh
+++ fitment/bin/build.sh
@@ -2,6 +2,6 @@ cd ..
 git pull
 
 mvn clean install -Dmaven.test.skip=true -Pweb
-cd blueprint-admin-web
+cd fitment-webserver
 rm -f webapp/WEB-INF/lib/*
 mvn dependency:copy-dependencies -DoutputDirectory=webapp/WEB-INF/lib -DincludeScope=runtime -Dsilent=true -DexcludeGroupIds=jspapi,javax.servlet
